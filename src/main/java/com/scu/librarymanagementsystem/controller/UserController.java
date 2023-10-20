771b09fcb2300d6ee1d49d490f028f533a162ead
GZK66666 <572223078@qq.com>
2023-10-20 09:49:10 +0800

    enable swagger

--- src/main/java/com/scu/librarymanagementsystem/controller/UserController.java
+++ src/main/java/com/scu/librarymanagementsystem/controller/UserController.java
@@ -3,6 +3,8 @@ package com.scu.librarymanagementsystem.controller;
 import com.scu.librarymanagementsystem.common.enums.UserType;
 import com.scu.librarymanagementsystem.model.User;
 import com.scu.librarymanagementsystem.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +15,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/Users")
+@Api(tags = "用户管理")
 public class UserController {
     @Autowired
     UserService userService;
 
     @GetMapping("/login")
+    @ApiOperation("登陆")
     @CrossOrigin // todo: remove when dev
     public String login(@RequestParam String userName, @RequestParam String passWord) {
         List<User> foundUsers = userService.findUsersByUserName(userName);
@@ -37,6 +41,7 @@ public class UserController {
 
     //    curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "userName=jbl&passWord=123456" http://localhost:8080/api/Users/add
     @PostMapping("/add")
+    @ApiOperation("新增用户")
     public ResponseEntity<String> addUser(@RequestParam String userName, @RequestParam String passWord) {
         Long id = userService.addUser(userName, passWord, UserType.USER); // todo: hardcode user type
 
@@ -48,26 +53,31 @@ public class UserController {
     }
 
     @GetMapping("/delete")
+    @ApiOperation("删除用户")
     public void deleteUser(@RequestParam String userName) {
         userService.deleteUser(userName);
     }
 
     @GetMapping("/updateUserName")
+    @ApiOperation("更新用户名")
     public int updateUserName(@RequestParam String oldUserName, @RequestParam String newUserName) {
         return userService.updateUserName(oldUserName, newUserName);
     }
 
     @GetMapping("/updatePassWord")
+    @ApiOperation("更新密码")
     public int updatePassWord(@RequestParam String userName, @RequestParam String newPassword) {
         return userService.updatePassword(userName, newPassword);
     }
 
     @GetMapping("/updateUserType")
+    @ApiOperation("更新用户类型")
     public int updateUserType(@RequestParam String userName, @RequestParam String userType) {
         return userService.updateUserType(userName, userType.equals("admin")?UserType.ADMIN:UserType.USER);
     }
 
     @GetMapping("/findUsersByUsernameAndUserType")
+    @ApiOperation("搜索用户")
     @CrossOrigin // todo: remove when dev
     public List<User> findUsersByUserNameAndUserType(@RequestParam String userName, @RequestParam String userType) {
         if (userName.isEmpty() && userType.isEmpty()) {
--- src/main/java/com/scu/librarymanagementsystem/model/User.java
+++ src/main/java/com/scu/librarymanagementsystem/model/User.java
@@ -1,6 +1,9 @@
 package com.scu.librarymanagementsystem.model;
 
 import com.scu.librarymanagementsystem.common.enums.UserType;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,12 +11,19 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@ApiModel("用户信息")
 public class User {
     @Id // 主键的意思
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 主键使用自增
     private Long id;
+
+    @ApiModelProperty("用户名")
     private String username;
+
+    @ApiModelProperty("密码")
     private String password;
+
+    @ApiModelProperty("用户类型")
     private UserType userType;
 
     public Long getId() {
