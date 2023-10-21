8f54905d389dd6bac2ef7daa47ecdad3b3c5ddaa
GZK66666 <572223078@qq.com>
2023-10-21 10:23:53 +0800

    use redis

---
+++ src/main/java/com/scu/librarymanagementsystem/common/utils/RedisUtil.java
@@ -0,0 +1,29 @@
+package com.scu.librarymanagementsystem.common.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class RedisUtil {
+    @Autowired
+    private final RedisTemplate redisTemplate;
+
+    public RedisUtil(RedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public void set(String key, Object value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    public void setWithExpiration(String key, Object value, long timeout, TimeUnit timeUnit) {
+        redisTemplate.opsForValue().set(key, value, timeout, timeUnit);
+    }
+
+    public Object get(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+}
--- src/main/java/com/scu/librarymanagementsystem/service/UserService.java
+++ src/main/java/com/scu/librarymanagementsystem/service/UserService.java
@@ -4,8 +4,6 @@ import com.scu.librarymanagementsystem.common.enums.UserType;
 import com.scu.librarymanagementsystem.model.User;
 import com.scu.librarymanagementsystem.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
